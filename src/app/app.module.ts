0b5569d9d30be862e642c80752eeeefd9d8da067
stware <stef.turrini@gmail.com>
2023-04-28 14:58:30 +0200

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { RouterModule } from '@angular/router';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { FirebaseModule } from 'src/firebase/firebase.module';
 
 @NgModule({
-  imports: [CommonModule, AngularMaterialModule],
+  imports: [CommonModule, AngularMaterialModule, FirebaseModule],
   declarations: [AppComponent, TopBarComponent, SidenavComponent],
   exports: [AppComponent, TopBarComponent],
   bootstrap: [AppComponent],
---
+++ src/firebase/firebase.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+export const firebase = {
+  apiKey: 'AIzaSyD9ltSJ8EBVeaiSDfk9u2N0MfhNcLOcS-w',
+  authDomain: 'wines-12389.firebaseapp.com',
+  databaseURL: 'https://wines-12389.firebaseapp.com',
+  projectId: 'wines-12389',
+  storageBucket: 'siddajmera-dev.appspot.com',
+  messagingSenderId: '483487617394',
+};
+
+@NgModule({
+  imports: [AngularFireModule.initializeApp(firebase), AngularFirestoreModule],
+  exports: [AngularFirestoreModule],
+})
+export class FirebaseModule {}
